a64accac4ea76e4eaa0daffd4722ed2b0c730f38
Johan Öbrink <johan.obrink@gmail.com>
2020-04-19 20:48:47 +0200

    Feature/vote (#15)
    * feat: 🎸 Refactoring and initial voting functionality
    
    * feat: 🎸 Voting and stats work
    
    * style: 💄 Added stricted lint and style rules
    
    * style: 💄 Added PR template

--- __test__/stores/tests.js
+++ __test__/stores/tests.js
@@ -1,6 +1,7 @@
 /* eslint-disable jest/no-export */
+const moment = require('moment')
 
-module.exports = ({ create, get, update, remove }) => {
+module.exports = ({ create, get, update, remove, save }) => {
   const id = 'cases/foo'
   afterEach(async () => await remove(id))
   describe('#create', () => {
@@ -15,8 +16,27 @@ module.exports = ({ create, get, update, remove }) => {
       const data = { herp: 'derp' }
       await create(id, data)
 
-      await expect(create(id, data))
-        .rejects.toThrow('Document exists')
+      await expect(create(id, data)).rejects.toThrow('Document exists')
+    })
+    it('returns id and writeTime', async () => {
+      const start = moment().unix()
+      const data = { herp: 'derp' }
+      const result = await create('cases/foo', data)
+      const end = moment().unix()
+
+      expect(result.id).toEqual('foo')
+      expect(result.writeTime.seconds).toBeGreaterThanOrEqual(start)
+      expect(result.writeTime.seconds).toBeLessThanOrEqual(end)
+    })
+    it('creates id if not supplied', async () => {
+      const start = moment().unix()
+      const data = { herp: 'derp' }
+      const result = await create('cases', data)
+      const end = moment().unix()
+
+      expect(result.id).toEqual(expect.any(String))
+      expect(result.writeTime.seconds).toBeGreaterThanOrEqual(start)
+      expect(result.writeTime.seconds).toBeLessThanOrEqual(end)
     })
   })
   describe('#update', () => {
@@ -32,8 +52,59 @@ module.exports = ({ create, get, update, remove }) => {
     it('throws if a document does not exist', async () => {
       const data = { herp: 'derp' }
 
-      await expect(update(id, data))
-        .rejects.toThrow('Document does not exist')
+      await expect(update(id, data)).rejects.toThrow('Document does not exist')
+    })
+    it('returns id and writeTime', async () => {
+      const data = { herp: 'derp' }
+      await create(id, data)
+
+      const start = moment().unix()
+      const result = await update('cases/foo', data)
+      const end = moment().unix()
+
+      expect(result.id).toEqual('foo')
+      expect(result.writeTime.seconds).toBeGreaterThanOrEqual(start)
+      expect(result.writeTime.seconds).toBeLessThanOrEqual(end)
+    })
+  })
+  describe('#save', () => {
+    it('creates a document', async () => {
+      const data = { herp: 'derp' }
+      await save(id, data)
+
+      const doc = await get(id)
+      expect(doc.data()).toEqual(data)
+    })
+    it('updates a document', async () => {
+      const data = { herp: 'derp' }
+      await create(id, data)
+      data.foo = 'bar'
+      await save(id, data)
+
+      const doc = await get(id)
+      expect(doc.data()).toEqual(data)
+    })
+    it('returns id and writeTime at create', async () => {
+      const start = moment().unix()
+      const data = { herp: 'derp' }
+      const result = await save('cases/foo', data)
+      const end = moment().unix()
+
+      expect(result.id).toEqual('foo')
+      expect(result.writeTime.seconds).toBeGreaterThanOrEqual(start)
+      expect(result.writeTime.seconds).toBeLessThanOrEqual(end)
+    })
+    it('returns id and writeTime at update', async () => {
+      const data = { herp: 'derp' }
+      await create(id, data)
+
+      const start = moment().unix()
+      const result = await save('cases/foo', data)
+      const end = moment().unix()
+
+      expect(result.id).toEqual('foo')
+      expect(result.writeTime.seconds).toBeGreaterThanOrEqual(start)
+      expect(result.writeTime.seconds).toBeLessThanOrEqual(end)
     })
   })
 }
